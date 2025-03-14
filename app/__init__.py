0dc393d6d197f31a4243b6796bb96e431f5d42bb
Matthew Gu <matthewgjy@gmail.com>
2025-03-15 00:58:44 +1100

    Add routes for group invite

--- app/__init__.py
+++ app/__init__.py
@@ -16,10 +16,12 @@ def create_app():
     from app.routes.api import api_bp
     from app.routes.auth import auth_bp
     from app.routes.group import group_bp
+    from app.routes.group_invite import group_invite_bp
 
     app.register_blueprint(api_bp, url_prefix="/api")
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(group_bp, url_prefix="/group")
+    app.register_blueprint(group_invite_bp, url_prefix="/invite")
 
     return app
 
---
+++ app/routes/group_invite.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, abort, jsonify, request
+
+from app.models.group_invite_model import GroupInviteModel
+
+group_invite_bp = Blueprint("group", __name__)
+
+
+@group_invite_bp.route("/", methods=["POST"])
+def invite():
+    try:
+        user_id = request.json["user_id"]
+        group_id = request.json["group_id"]
+        GroupInviteModel.create_group_invite(user_id, group_id)
+        return jsonify({"msg": "Invited user successfully"}), 200
+    except ValueError as ve:
+        abort(400, str(ve))
+    except Exception as e:
+        abort(500, "Internal Server Error")
+
+
+@group_invite_bp.route("accept", methods=["POST"])
+def accept():
+    try:
+        user_id = request.json["user_id"]
+        group_id = request.json["group_id"]
+        GroupInviteModel.accept_group_invite(user_id, group_id)
+        return jsonify({"msg": "Accepted group invite successfully"}), 200
+    except ValueError as ve:
+        abort(400, str(ve))
+    except Exception as e:
+        abort(500, "Internal Server Error")
+
+
+@group_invite_bp.route("reject", methods=["POST"])
+def reject():
+    try:
+        user_id = request.json["user_id"]
+        group_id = request.json["group_id"]
+        GroupInviteModel.reject_group_invite(user_id, group_id)
+        return jsonify({"msg": "Rejected group invite successfully"}), 200
+    except ValueError as ve:
+        abort(400, str(ve))
+    except Exception as e:
+        abort(500, "Internal Server Error")
