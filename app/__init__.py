f67695c735db9c2732aada74afb9b4a1b55ec4d8
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-14 20:56:33 +1100

    Added routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 from flask import Flask
 
+from app.routes.api import api_bp
+from app.routes.auth import auth_bp
+
 def create_app():
     app = Flask(__name__)
 
-    @app.route("/")
-    def hello_world():
-        return "<p>Hello, World!</p>"
-
-
+    app.register_blueprint(api_bp, url_prefix='/api')
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
