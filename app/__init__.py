47c984b5edf38ccdfbc76dd3e5c5b6afbcfda889
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-14 22:34:43 +1100

    Added config class and dotenv

--- app/__init__.py
+++ app/__init__.py
@@ -5,12 +5,17 @@ from flask_pymongo import PyMongo
 from app.routes.api import api_bp
 from app.routes.auth import auth_bp
 
+<<<<<<< HEAD
 bcrypt = Bcrypt()
+=======
+from app.config import Config
+
+>>>>>>> 686dd07 (Added config class and dotenv)
 mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object('app.config')
+    app.config.from_object(Config)
 
     bcrypt.init_app(app)
     mongo.init_app(app)
