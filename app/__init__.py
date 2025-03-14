ba47b0f4e9ebe82fd8f7e3df1e6b7a3cfa1d0128
Matthew Gu <matthewgjy@gmail.com>
2025-03-14 21:47:44 +1100

    Add database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from flask_pymongo import PyMongo
 
 from app.routes.api import api_bp
 from app.routes.auth import auth_bp
 
+mongo = PyMongo()
+
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('app.config')
 
+    mongo.init_app(app)
+    
     app.register_blueprint(api_bp, url_prefix='/api')
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
+
+def get_db():
+    return mongo.db
