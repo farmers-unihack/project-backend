28e06b5e4072da93e551398692c05c4f0ce80e0a
Matthew Gu <matthewgjy@gmail.com>
2025-03-14 22:54:56 +1100

    Update task to track completion and completion time

--- app/models/task_model.py
+++ app/models/task_model.py
@@ -1,47 +1,60 @@
 from bson.objectid import ObjectId
-from app import get_db
 from pymongo.collection import Collection
+
+from app import get_db
+from app.utils.time import get_current_time
+
 
 class TaskModel:
     def __init__(self):
         self.collection: Collection = get_db().tasks
 
-    def create_task(self, name, description):
+    def create_task(self, name: str, description: str = ""):
         task = {
             "name": name,
             "description": description,
+            "completion_date": None,
+            "completed": False,
         }
         result = self.collection.insert_one(task)
         return result.inserted_id
 
-    def get_task(self, task_id):
+    def get_task(self, task_id: str):
         try:
             return self.collection.find_one({"_id": ObjectId(task_id)})
         except Exception as e:
             print(f"Error getting task: {e}")
             return None
 
-    def update_task(self, task_id, title=None, description=None):
+    def update_task(
+        self,
+        task_id: str,
+        name: str = None,
+        description: str = None,
+        completed: bool = None,
+    ):
         update_fields = {}
-        if title:
-            update_fields["title"] = title
+        if name:
+            update_fields["name"] = name
         if description:
             update_fields["description"] = description
+        if completed is not None:
+            update_fields["completed"] = completed
+            update_fields["completion_date"] = get_current_time() if completed else None
         if update_fields:
             try:
-                self.collection.update_one({"_id": ObjectId(task_id)}, {"$set": update_fields})
+                updated = self.collection.update_one(
+                    {"_id": ObjectId(task_id)}, {"$set": update_fields}
+                )
+                if updated.modified_count == 0:
+                    raise ValueError("Task not found")
+            except ValueError as ve:
+                print(f"Error updating task: {ve}")
             except Exception as e:
-                print(f"Error updating task: {e}")
+                print(f"Unexpected error updating task: {e}")
 
-    def delete_task(self, task_id):
+    def delete_task(self, task_id: str):
         try:
             self.collection.delete_one({"_id": ObjectId(task_id)})
         except Exception as e:
             print(f"Error deleting task: {e}")
-
-    def get_all_tasks(self):
-        try:
-            return list(self.collection.find())
-        except Exception as e:
-            print(f"Error getting all tasks: {e}")
-            return []
