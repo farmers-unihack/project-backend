146167913f2a78392ba9781e50c6d643cbe98749
Matthew Gu <matthewgjy@gmail.com>
2025-03-15 00:45:46 +1100

    Update config to match the expected URI variable name

--- app/config.py
+++ app/config.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    MONGODB_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/unihack')
+    MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/unihack')
