1723e781d9d5e1edf26485429b227a847d157fad
Matthew Gu <matthewgjy@gmail.com>
2025-03-14 21:47:44 +1100

    Add database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,24 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_pymongo import PyMongo
 
 from app.routes.api import api_bp
 from app.routes.auth import auth_bp
 
 bcrypt = Bcrypt()
+mongo = PyMongo()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object('app.config')
 
     bcrypt.init_app(app)
-
+    mongo.init_app(app)
+    
     app.register_blueprint(api_bp, url_prefix='/api')
     app.register_blueprint(auth_bp, url_prefix='/auth')
 
     return app
+
+def get_db():
+    return mongo.db
---
+++ app/config.py
@@ -0,0 +1,3 @@
+import os
+
+MONGODB_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/unihack')
