cc82d3bcf071f46e90198f0194bc1b32e4bcbcb0
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-15 21:48:24 +1100

    Added poll endpoint

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -3,8 +3,9 @@ class User():
     def __init__(self, user_data: dict):
         self.id = str(user_data["_id"])
         self.username = user_data["username"] 
-        self.hashed_password = user_data["hashed_password"] 
+        self.hashed_password = user_data.get("hashed_password", None) 
         self.clock_in_timestamp = user_data.get("clock_in_timestamp", None)
+        self.sessions = user_data.get("sessions", [])
 
     def is_clocked_in(self) -> bool:
-        return not (self.clock_in_timestamp == None)
+        return self.clock_in_timestamp != None
