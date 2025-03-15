1629e112dc351b77476c5a538e2fcf2b6a483874
Matthew Gu <matthewgjy@gmail.com>
2025-03-15 16:12:46 +1100

    Add recently completed task endpoint

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -4,11 +4,13 @@ from pymongo.database import Database
 
 from app.controllers.auth_controller import create_auth_bp
 from app.controllers.group_controller import create_group_bp
+from app.controllers.task_controller import create_task_bp
 from app.repositories.group_repository import GroupRepository
 from app.repositories.task_repository import TaskRepository
 from app.repositories.user_repository import UserRepository
 from app.services.auth_service import AuthService
 from app.services.group_service import GroupService
+from app.services.task_service import TaskService
 from app.services.user_service import UserService
 
 def register_controllers(app: Flask, db: Database, bcrypt: Bcrypt):
@@ -21,13 +23,18 @@ def register_controllers(app: Flask, db: Database, bcrypt: Bcrypt):
     user_service = UserService(user_repository)
     group_service = GroupService(group_repository, user_repository)
     auth_service = AuthService(app, user_repository, bcrypt)
+    task_service = TaskService(task_repository)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return user_repository.find_by_id(user_id)
 
     # Controllers
     auth_bp = create_auth_bp(auth_service)
     group_bp = create_group_bp(group_service)
+    task_bp = create_task_bp(task_service)
 
     # Register blueprints
     app.register_blueprint(auth_bp, url_prefix="/api/auth")
     app.register_blueprint(group_bp, url_prefix="/api/group")
-
-
+    app.register_blueprint(task_bp, url_prefix="/api/task")
--- app/controllers/group_controller.py
+++ app/controllers/group_controller.py
@@ -1,10 +1,13 @@
 from flask import Blueprint, abort, jsonify, request
+from app.models.task_model import Task
 from app.services.group_service import GroupService
-from app.utils.request_checker import safe_json 
+from app.utils.request_checker import safe_json
 import traceback
 
 
-def create_group_bp(group_service: GroupService) -> Blueprint:
+def create_group_bp(
+    group_service: GroupService, task_service: TaskService, user_service: UserService
+) -> Blueprint:
     group_bp = Blueprint("group", __name__)
 
     @group_bp.route("create", methods=["POST"])
@@ -58,17 +61,21 @@ def create_group_bp(group_service: GroupService) -> Blueprint:
             traceback.print_exc()
             abort(500, "Internal Server Error")
 
-    @group_bp.route("completed_tasks", methods=["GET"])
-    def completed_tasks():
-        abort(500, "Not Implemented")
-        # try:
-        #     group_id = request.args.get("group_id")
-        #     time_limit = request.args.get("time_limit")
-        #     tasks = group_service.get_completed_tasks(group_id, time_limit)
-        #     return jsonify(tasks), 200
-        # except ValueError as ve:
-        #     abort(400, str(ve))
-        # except Exception as e:
-        #     abort(500, "Internal Server Error")
+    @group_bp.route("recent_completed_tasks", methods=["GET"])
+    def recent_completed_tasks():
+        try:
+            group_id = safe_json("group_id")
+            time_limit = safe_json("time_limit")
+            group_tasks: list[Task] = group_service.get_group_tasks(group_id)
+            filtered_tasks = filter(
+                lambda task: task.is_completed_within_recent_time(time_limit),
+                group_tasks,
+            )
+            return jsonify(filtered_tasks), 200
+        except ValueError as ve:
+            abort(400, str(ve))
+        except Exception:
+            traceback.print_exc()
+            abort(500, "Internal Server Error")
 
     return group_bp
