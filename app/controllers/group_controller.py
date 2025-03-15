d217554251c5515281a2211122d3f4e20915f882
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-15 16:12:41 +1100

    Added protection for json gets

--- app/controllers/group_controller.py
+++ app/controllers/group_controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, abort, jsonify, request
 from app.services.group_service import GroupService
-from app.utils.request_checker import request_json_contains
+from app.utils.request_checker import safe_json 
+import traceback
 
 
 def create_group_bp(group_service: GroupService) -> Blueprint:
@@ -9,57 +10,52 @@ def create_group_bp(group_service: GroupService) -> Blueprint:
     @group_bp.route("create", methods=["POST"])
     def create():
         try:
-            if not request_json_contains(("group_name", "user_id")):
-                abort(400, "Invalid request")
-            group_name = request.json["group_name"]
-            user_id = request.json["user_id"]
+            group_name = safe_json("group_name")
+            user_id = safe_json("user_id")
             group_service.create_group(group_name, user_id)
             return jsonify({"msg": "Group created successfully"}), 201
         except ValueError as ve:
             abort(400, str(ve))
-        except Exception as e:
-            print(e)
+        except Exception:
+            traceback.print_exc()
             abort(500, "Internal Server Error")
 
     @group_bp.route("join", methods=["POST"])
     def join():
         try:
-            if not request_json_contains(("group_id", "user_id")):
-                abort(400, "Invalid request")
-            user_id = request.json["user_id"]
-            group_id = request.json["group_id"]
+            user_id = safe_json("user_id")
+            group_id = safe_json("group_id")
             group_service.add_user_to_group(user_id, group_id)
             return jsonify({"msg": "Joined group successfully"}), 200
         except ValueError as ve:
             abort(400, str(ve))
-        except Exception as e:
+        except Exception:
+            traceback.print_exc()
             abort(500, "Internal Server Error")
 
     @group_bp.route("users", methods=["GET"])
     def users():
         try:
-            if not request_json_contains(("group_id")):
-                abort(400, "Invalid request")
             group_id = request.args.get("group_id")
             group = group_service.find_group_by_id(group_id)
             return jsonify(group.users), 200
         except ValueError as ve:
             abort(400, str(ve))
-        except Exception as e:
+        except Exception:
+            traceback.print_exc()
             abort(500, "Internal Server Error")
 
     @group_bp.route("leave", methods=["POST"])
     def leave_group():
         try:
-            if not request_json_contains(("group_id", "user_id")):
-                abort(400, "Invalid request")
-            user_id = request.json["user_id"]
-            group_id = request.json["group_id"]
+            user_id = safe_json("user_id")
+            group_id = safe_json("group_id")
             group_service.remove_user_from_group(user_id, group_id)
             return jsonify({"msg": "Left group successfully"}), 200
         except ValueError as ve:
             abort(400, str(ve))
-        except Exception as e:
+        except Exception:
+            traceback.print_exc()
             abort(500, "Internal Server Error")
 
     @group_bp.route("completed_tasks", methods=["GET"])
--- app/utils/request_checker.py
+++ app/utils/request_checker.py
@@ -1,4 +1,11 @@
 from flask import request
 
-def request_json_contains(required_fields: tuple[str]) -> bool:
-    return request.json != None and not all(k in request.json for k in required_fields)
+def safe_json(field_name: str) -> str:
+    if request.json == None:
+        raise ValueError("No json provided in the request")
+
+    if field_name not in request.json:
+        raise ValueError(f"The field {field_name} was not provided.")
+
+    return request.json[field_name]
+
