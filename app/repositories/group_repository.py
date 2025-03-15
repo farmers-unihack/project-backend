c8960df53769a29274dde7a94d10c4375ef610bc
Matthew Gu <matthewgjy@gmail.com>
2025-03-15 11:06:17 +1100

    Update group routes and model for creation and deletion logic

--- app/repositories/group_repository.py
+++ app/repositories/group_repository.py
@@ -2,17 +2,19 @@ from typing import Optional
 from bson.objectid import ObjectId
 from pymongo.database import Database
 from pymongo.results import UpdateResult
+from pymongo.results import DeleteResult
 
 from app.models.group_model import Group
 
-class GroupRepository():
+
+class GroupRepository:
     def __init__(self, db: Database) -> None:
         self.db = db
 
-    def create_group(self, name: str) -> Optional[Group]:
+    def create_group(self, name: str, user_id: str) -> Optional[Group]:
         group_data = {
             "name": name,
-            "users": [],
+            "users": [user_id],
         }
 
         result = self.db.groups.insert_one(group_data)
@@ -21,14 +23,21 @@ class GroupRepository():
             return None
 
         group_data["_id"] = result.inserted_id
-        return Group(group_data) 
+        return Group(group_data)
 
     def find_group_by_id(self, group_id: str) -> Optional[Group]:
         group_data = self.db.groups.find_one({"_id": ObjectId(group_id)})
-        return Group(group_data) if group_data else None 
+        return Group(group_data) if group_data else None
 
     def add_user_to_group(self, group_id: str, user_id: str) -> UpdateResult:
-        return self.db.groups.update_one( {"_id": ObjectId(group_id)}, {"$push": { "users": user_id }} )
+        return self.db.groups.update_one(
+            {"_id": ObjectId(group_id)}, {"$push": {"users": user_id}}
+        )
 
     def remove_user_from_group(self, group_id: str, user_id: str) -> UpdateResult:
-        return self.db.groups.update_one( {"_id": ObjectId(group_id)}, {"$pull": { "users": user_id }} )
+        return self.db.groups.update_one(
+            {"_id": ObjectId(group_id)}, {"$pull": {"users": user_id}}
+        )
+
+    def delete_group_by_id(self, group_id: str) -> DeleteResult:
+        return self.db.groups.delete_one({"_id": ObjectId(group_id)})
--- app/services/group_service.py
+++ app/services/group_service.py
@@ -1,13 +1,19 @@
 from typing import Optional
 from app.models.group_model import Group
-from app.repositories import user_repository
 from app.repositories.group_repository import GroupRepository
 from app.repositories.user_repository import UserRepository
 
+
 class GroupService:
-    def __init__(self, group_repository: GroupRepository, user_repository: UserRepository) -> None:
+    def __init__(
+        self, group_repository: GroupRepository, user_repository: UserRepository
+    ) -> None:
         self.group_repository = group_repository
         self.user_repository = user_repository
+
+    def create_group(self, group_name: str, user_id: str) -> Optional[Group]:
+        # TODO: check if the user is already in a group
+        return self.group_repository.create_group(group_name, user_id)
 
     def add_user_to_group(self, group_id: str, user_id: str) -> tuple[bool, str]:
         group = self.group_repository.find_group_by_id(group_id)
@@ -33,9 +39,11 @@ class GroupService:
         if not self.user_repository.find_by_id(user_id):
             return (False, "User does not exist")
 
-        if group.get_member_count() == 1:
-            # TODO handle group deletion if last member leaves
-            pass
+        if group.get_member_count() == 1 and group.contains_user(user_id):
+            self.group_repository.delete_group_by_id(group_id)
+            return (True, "Group deleted as last member left")
 
-        self.group_repository.remove_user_from_group(group_id, user_id)
+        delete_result = self.group_repository.remove_user_from_group(group_id, user_id)
+        if delete_result.modified_count == 0:
+            return (False, "User not in group")
         return (True, "User remove from group")
