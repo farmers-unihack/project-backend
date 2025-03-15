96d8700539a757ad977cd42e4e180c814e318ee4
Matthew Gu <matthewgjy@gmail.com>
2025-03-15 22:04:13 +1100

    Change seconds to hours

--- app/models/collectible_model.py
+++ app/models/collectible_model.py
@@ -2,7 +2,7 @@ from typing import Any
 
 
 class Collectible:
-    PER_COLLECTIBLE_TIME_INCREMENT = 5
+    PER_COLLECTIBLE_TIME_INCREMENT = 5 * 3500 # in seconds
 
     def __init__(self, data: dict[str, Any]) -> None:
         self.id = data["_id"]
