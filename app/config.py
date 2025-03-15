d66a63a47482df3f8d3567033974c35b66e3ef64
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-15 15:21:12 +1100

    Removed config values for session management

--- app/config.py
+++ app/config.py
@@ -7,6 +7,3 @@ class Config:
     MONGO_URI = os.environ.get('MONGO_URI', 'mongodb://localhost:27017/unihack')
     ENV = os.environ.get('ENV', 'development')
     SECRET_KEY = os.environ.get('SECRET_KEY', "supersecretkey")
-    SESSION_TYPE = os.environ.get('SESSION_TYPE', 'filesystem')
-    SESSION_PERMANENT = os.environ.get('SESSION_PERMANENT', False)
-    SESSION_USE_SIGNER = os.environ.get('SESSION_USE_SIGNER', True)
