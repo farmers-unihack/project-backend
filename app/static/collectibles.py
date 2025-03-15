e7bb15fad0f854ab30a70b600d60b0f9a9b54eb7
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-16 04:18:16 +1100

    Added all the collectibles

--- app/static/collectibles.py
+++ app/static/collectibles.py
@@ -1,9 +1,28 @@
 PER_COLLECTIBLE_TIME_INCREMENT = 10 * 60  # 10 mintues 
 
 ALL_COLLECTIBLES = {
-    "collectible_a": {
-        "display_name": "Collectable A",
+    "collectible_1": {
+        "id": "collectible_1",
         "description": "This is test collectible A",
-        "image_url": "collectible_a.jpg",
+    },
+    "collectible_2": {
+        "id": "collectible_2",
+        "description": "This is test collectible B",
+    },
+    "collectible_3": {
+        "id": "collectible_3",
+        "description": "This is test collectible C",
+    },
+    "collectible_4": {
+        "id": "collectible_4",
+        "description": "This is test collectible D",
+    },
+    "collectible_5": {
+        "id": "collectible_5",
+        "description": "This is test collectible E",
+    },
+    "collectible_6": {
+        "id": "collectible_6",
+        "description": "This is test collectible F",
     },
 }
