6ea15bceaf7517eb95e24c62f6cd889649b8c800
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-15 15:24:22 +1100

    Removed all the previous login management system depending on sessions

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,8 +1,7 @@
-from flask_login import UserMixin
 
 
-class User(UserMixin):
+class User():
     def __init__(self, user_data):
-        self.id = user_data["_id"]
+        self.id = str(user_data["_id"])
         self.username = user_data["username"] 
         self.hashed_password = user_data["hashed_password"] 
