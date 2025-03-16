cfac923d6525fec2b9ed3e4865974eff7beeb571
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-16 12:56:40 +1100

    fix none problem with users

--- app/models/user_model.py
+++ app/models/user_model.py
@@ -3,8 +3,7 @@ import datetime
 
 class User:
     def __init__(self, user_data: dict):
-        print(user_data)
-        self.id = user_data["_id"]
+        self.id = str(user_data["_id"])
         self.username = user_data["username"]
         self.hashed_password = user_data.get("hashed_password", None)
         self.clock_in_timestamp = user_data.get("clock_in_timestamp", None)
