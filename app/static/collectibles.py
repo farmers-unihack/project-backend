d67bffb86e44871061b11c090d526b04cf6f9e40
TewPingz <38973872+TewPingz@users.noreply.github.com>
2025-03-16 15:09:27 +1100

    add correct collectibles

--- app/static/collectibles.py
+++ app/static/collectibles.py
@@ -1,10 +1,28 @@
 PER_COLLECTIBLE_TIME_INCREMENT = 1 * 60  # 1 mintue
 
-ALL_COLLECTIBLES: dict[str, dict] = {
-    "Item 1": {"description": "A very nice item"},
-    "Item 2": {"description": "Second best item"},
-    "Item 3": {"description": "Third best item"},
-    "Item 4": {"description": "Fourth best item"},
-    "Item 5": {"description": "Fifth best item"},
-    "Item 6": {"description": "Sixth best item"},
+ALL_COLLECTIBLES = {
+     "collectible_1": {
+         "id": "collectible_1",
+         "description": "This is test collectible A",
+     },
+     "collectible_2": {
+         "id": "collectible_2",
+         "description": "This is test collectible B",
+     },
+     "collectible_3": {
+         "id": "collectible_3",
+         "description": "This is test collectible C",
+     },
+     "collectible_4": {
+         "id": "collectible_4",
+         "description": "This is test collectible D",
+     },
+     "collectible_5": {
+         "id": "collectible_5",
+         "description": "This is test collectible E",
+     },
+     "collectible_6": {
+         "id": "collectible_6",
+         "description": "This is test collectible F",
+     },
 }
